01083ab5ec885f8deb97e19c8754d3b88fe96dfd
rmndzn <72603858+rmndzn@users.noreply.github.com>
2026-03-18 19:18:19 +0800

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/animandexv2",
   server: {
     port: 5173
   }
